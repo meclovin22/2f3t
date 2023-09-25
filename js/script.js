8427ff947fc7eb9c455c4bb7e4601a8767ed4ad2
a <108934829+Luizfernandouu@users.noreply.github.com>
2023-09-25 08:02:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-mensagem=(msg)  => alert(msg);
 
+
+mensagem = (msg) => alert(msg);
 soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = x => Math.sqrt(x);
