595c52933eeb89e60aaf40896795b6a2392d3688
a <108934829+Luizfernandouu@users.noreply.github.com>
2023-10-09 08:28:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,82 @@
-
-
 mensagem = (msg) => alert(msg);
 soma = (a,b) => a + b;
 sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
 raiz = x => Math.sqrt(x);
+
+let a = "";
+let b = "";
+let valor = "";
+let temPonto = false;
+let desligada = true;
+
+function porcentagem(){
+if(executa == "mult"){
+b = valor;
+escrever_display(div(mult(a,b),100));
+b = "";
+valor = "";
+
+}
+
+}
+
+function raiz_quadrada(){
+    escrever_display(raiz(valor));
+    valor = "";
+}
+
+function onoff(){
+    if(desligada){
+        escrever_display("");
+        desligada = false;
+    }else{
+        zerar();
+        desligada = true;
+    }
+}
+
+function zerar(){
+a = "";
+ b = "";
+valor = "";
+temPonto = "";
+escrever_display(0)
+
+}
+function escrever_display(num){
+    document.getElementById("resultado").value=num;
+}
+function digitando(tecla){
+   if( tecla == "."){
+        if(!temPonto){
+            temPonto = true;
+            valor += tecla;
+            escrever_display(valor);
+        }
+        return;
+   }
+   valor += tecla;
+   escrever_display(valor);
+}
+let executa = "";
+function operacao(op){
+    executa = op;
+    a = valor;
+    valor = "";
+    temPonto = false;
+}
+function calcula(){
+    if(executa != ""){
+        b=valor;
+        if(executa == "mult") escrever_display(mult(a,b));
+        if(executa == "div") escrever_display(div(a,b));
+        if(executa == "sub") escrever_display(sub(a,b));
+        if(executa == "soma") escrever_display(soma(Number(a),Number(b)));
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
+    }
+}
